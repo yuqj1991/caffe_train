2736a3bdcafc1f79aacac953f06aa2005880e173
tonny Yu <tonny.yu@honeywell.com>
2019-03-07 18:20:54 +0800

    ....

--- examples/deepano_face/face_detector/scripts/deepano_train.sh
+++ examples/deepano_face/face_detector/scripts/deepano_train.sh
@@ -9,4 +9,4 @@ if ! test -f ../prototxt/deepano_light_face_test.prototxt ;then
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train -solver="../solver/deepano_solver_train.prototxt" -gpu 0
+../../../../build/tools/caffe train --solver=../solver/deepano_solver_train.prototxt --snapshot=/home/resideo/workspace/deepano_face_train/examples/deepano_face/face_detector/snapshot/deepanoFace_iter_60000.solverstate -gpu 0
