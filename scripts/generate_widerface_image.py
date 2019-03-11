3184b0a8647ef9122c99495549038b48f5064ec8
tonny Yu <tonny.yu@honeywell.com>
2019-03-12 03:44:54 +0800

    ...boxes size

--- scripts/generate_widerface_image.py
+++ scripts/generate_widerface_image.py
@@ -11,13 +11,14 @@ from xml.dom.minidom import Document
 # from lxml.etree import Element, SubElement, tostring
 sys.setrecursionlimit(1000000)
 
-SOURCE_IMAGE_ROOT = '../'
-ANNOTATIONS_DIR = '../wider_face/Annotations'
-LABEL_DIR = '../wider_face/label'
+ANNOTATIONS_DIR = '../../dataset/facedata/wider_face/Annotations'
+LABEL_DIR = '../../dataset/facedata/wider_face/label'
 wider_directory = ['wider_train', 'wider_test', 'wider_val']
-Annotation_img_dir = '../wider_face/Annotation_img'
-root_dir = "../../../dataset/facedata/"
+Annotation_img_dir = '../../dataset/facedata/wider_face/Annotation_img'
+root_dir = "../../dataset/facedata/"
 anno_src_wider_dir = ['wider_face_train_bbx_gt.txt', 'wider_face_val_bbx_gt.txt']
+height_level = [120, 240, 360, 480, 600, 720, 840, 960, 1080, 1200, 1320, 1440,9000]
+thread_hold = 30
 class ConfigureHistogram(object):
 	def __init__(self):
 		self.count = 0
@@ -30,6 +31,7 @@ class ConfigureHistogram(object):
 		self.face_pose = []
 		self.face_occlusion = []
 		self.face_illumination = []
+		self.face_width_aspect_ratio = []
 
 	def face_append(self, width, height, blur, pose, occlusion, illumination):
 		self.face_count += 1
@@ -39,6 +41,14 @@ class ConfigureHistogram(object):
 		self.face_pose.append(pose)
 		self.face_occlusion.append(occlusion)
 		self.face_illumination.append(illumination)
+
+	def face_specfic_append(self, width, height, blur, occlusion, aspect_ratio):
+		self.face_count += 1
+		self.face_width.append(width)
+		self.face_height.append(height)
+		self.face_blur.append(blur)
+		self.face_occlusion.append(occlusion)
+		self.face_width_aspect_ratio.append(aspect_ratio)
 
 	def image_append(self, img_width, img_height):
 		self.count += 1
@@ -53,11 +63,10 @@ def static_histogram_data_from_all_sample(source_folder,label_folder):
 	static_his = ConfigureHistogram()
 	for sub_folder in sub_image_folder:
 		file_list = os.listdir(source_folder+'/'+sub_folder)
-
 		for image_file in file_list:
 			image_file_full_path = source_folder+'/'+sub_folder+'/'+image_file
 			# print(image_file_full_path)
-			xml_file = xml.dom.minidom.parse('../wider_face/Annotations/'+image_file.split('.')[-2]+'.xml')
+			xml_file = xml.dom.minidom.parse('../../dataset/facedata/wider_face/Annotations/'+image_file.split('.')[-2]+'.xml')
 			# print('../wider_face/Annotations/'+image_file.split('.')[-2]+'.xml')
 			root = xml_file.documentElement
 			width = root.getElementsByTagName('width')
@@ -84,12 +93,119 @@ def static_histogram_data_from_all_sample(source_folder,label_folder):
 					static_his.face_append(int(face_width), int(face_height), int(blur), int(pose), int(occlusion), int(illumination))
 					if int(face_width)<=0 or int(face_height)<=0:
 						print('face_width, and height:',int(face_width), int(face_height))
-						print('../wider_face/Annotations/'+image_file.split('.')[-2]+'.xml')
+						print('../../dataset/facedata/wider_face/Annotations/'+image_file.split('.')[-2]+'.xml')
 					lab_an_line = lab_file.readline()
 			if img_height > 4500:
-				print('../wider_face/Annotations/'+image_file.split('.')[-2]+'.xml')
+				print('../../dataset/facedata/wider_face/Annotations/'+image_file.split('.')[-2]+'.xml')
 				print('img_height:', img_height)
 	return static_his
+
+
+# statsic specfic image height range
+def statsic_specfic_face_base_data(source_folder,label_folder, heightMin, heightMax):
+	# source_folder: ../wider_face/JPEGImages/wider_train/images
+	# label_folder: ../wider_face/label
+	assert heightMax > heightMin
+	print("heightMin, %d ,heightMax %d"%(heightMin, heightMax))
+	sub_image_folder = os.listdir(source_folder)
+	static_his = ConfigureHistogram()
+	for sub_folder in sub_image_folder:
+		file_list = os.listdir(source_folder+'/'+sub_folder)
+		for image_file in file_list:
+			image_file_full_path = source_folder+'/'+sub_folder+'/'+image_file
+			xml_file = xml.dom.minidom.parse('../../dataset/facedata/wider_face/Annotations/'+image_file.split('.')[-2]+'.xml')
+			root = xml_file.documentElement
+			width = root.getElementsByTagName('width')
+			img_width = int(width[0].firstChild.data)
+			height = root.getElementsByTagName('height')
+			img_height = int(height[0].firstChild.data)
+			if img_height >=heightMin and img_height < heightMax:
+				static_his.image_append(img_width, img_height)
+				label_file = label_folder+'/'+image_file.split('.')[-2]
+				with open(label_file, 'r') as lab_file:
+					lab_an_line = lab_file.readline()
+					while lab_an_line:
+						anno_str_bbox = lab_an_line.split(' ')
+						x_min = anno_str_bbox[0]
+						y_min = anno_str_bbox[1]
+						face_width = anno_str_bbox[2]
+						face_height = anno_str_bbox[3]
+						blur = anno_str_bbox[4]
+						occlusion = anno_str_bbox[5]
+						aspec_ratio =float(int(face_width)/int(face_height))
+						static_his.face_specfic_append(int(face_width), int(face_height), int(blur), int(occlusion), aspec_ratio)
+						if int(face_width)<=0 or int(face_height)<=0:
+							print('face_width, and height:',int(face_width), int(face_height))
+							print('../../dataset/facedata/wider_face/Annotations/'+image_file.split('.')[-2]+'.xml')
+						lab_an_line = lab_file.readline()
+	return static_his
+
+
+def draw_histogram_specfic_range_base_data():
+	face_num=0
+	for ii in range(len(height_level)):
+		if ii == len(height_level)-1:
+			break
+		static_data = statsic_specfic_face_base_data('../../dataset/facedata/wider_face/JPEGImages/wider_train/images', '../../dataset/facedata/wider_face/label',
+													 height_level[ii], height_level[ii+1])
+		img_num = static_data.count
+		face_num += static_data.face_count
+		print('img_num: ', img_num)
+		img_face_num = static_data.face_count
+		# height_min and _max
+		img_height_min = np.min(static_data.height)
+		img_height_max = np.max(static_data.height)
+		# face_width_min and _max
+		face_width_min = np.min(static_data.face_width)
+		face_width_max = np.max(static_data.face_width)
+		# face_height_min and _max
+		face_height_min = np.min(static_data.face_height)
+		face_height_max = np.max(static_data.face_height)
+		# face width/height aspec_ratio
+		face_aspec_ratio_min = np.min(static_data.face_width_aspect_ratio)
+		face_aspec_ratio_max = np.max(static_data.face_width_aspect_ratio)
+		# print img_height and img_width(min and max), and face_height and width (min and max)
+		print('img height min & max ', img_height_min, img_height_max)
+		print('img_face width min & max ', face_width_min, face_width_max)
+		print('img_face height min & max ', face_height_min, face_height_max)
+		print('face_aspec_ratio_ min & max ', face_aspec_ratio_min, face_aspec_ratio_max)
+		# i want to know the img height disturibution , so i use histogram, and i need to make some gap between the min and max of img_height
+		# and by the max gap between the max and the min ,i want to make 120 gap,year
+		# ==========================================================
+		# face_width min & max, and i will set gap between min & max
+		# ==========================================================
+		face_width_iter = math.ceil(float(976)/90)
+		createVar = locals()
+		for ii in range(int(face_width_iter)):
+			createVar['face_width_levle_gap_'+str(ii*90)] =0
+		for ii in range(len(static_data.face_width)):
+			for jj in range(int(face_width_iter)):
+				if 90*(jj) <= static_data.face_width[ii] < 90*(jj+1):
+					createVar['face_width_levle_gap_'+str(jj*90)] += 1
+					break
+		face_width_list = []
+		for ii in range(int(face_width_iter)):
+			if createVar['face_width_levle_gap_'+str(ii*90)]>0:
+				face_width_list.append(createVar['face_width_levle_gap_'+str(ii*90)])
+				print('face_width_levle_gap_'+str(ii*90),createVar['face_width_levle_gap_'+str(ii*90)])
+		# ===============================================================
+		# face_height min & max, and i will set gap between min & max too
+		# ===============================================================
+		face_height_iter = math.ceil(float(1289)/120)
+		for ii in range(int(face_height_iter)):
+			createVar['face_height_levle_gap_'+str(ii*120)] =0
+		for ii in range(len(static_data.face_height)):
+			for jj in range(int(face_height_iter)):
+				if 120*(jj) <= static_data.face_height[ii] < 120*(jj+1):
+					createVar['face_height_levle_gap_'+str(jj*120)] += 1
+					break
+		face_height_list = []
+		for ii in range(int(face_height_iter)):
+			if createVar['face_height_levle_gap_'+str(ii*120)]>0:
+				face_height_list.append(createVar['face_height_levle_gap_'+str(ii*120)])
+				print('face_height_levle_gap_'+str(ii*120),createVar['face_height_levle_gap_'+str(ii*120)])
+		print('####################################################')
+	print("face_num:", face_num)
 
 
 def autolabel(rects):
@@ -99,7 +215,7 @@ def autolabel(rects):
 
 
 def draw_histogram_base_data():
-	static_data = static_histogram_data_from_all_sample('../wider_face/JPEGImages/wider_train/images', '../wider_face/label')
+	static_data = static_histogram_data_from_all_sample('../../dataset/facedata/wider_face/JPEGImages/wider_train/images', '../../dataset/facedata/wider_face/label')
 	img_num = static_data.count
 	img_face_num = static_data.face_count
 	# width_min and _max
@@ -268,7 +384,7 @@ def load_wider_split(split_file):
 				newline = x_min + ' '+y_min+ ' '+width+ ' '+height+ ' '+blur+ ' '+occlusion+' \n'
 				if int(invalid) == 1:
 					continue
-				if int(width)<30 or int(height)<30:
+				if int(width)< thread_hold or int(height)< thread_hold:
 					continue
 				lab_file.writelines(newline)
 			lab_file.close()
@@ -370,11 +486,7 @@ def generate_xml_from_wider_face(label_source_folder, img_filename, xml_save_fol
 			cv2.rectangle(source_img, (int(x_min), int(y_min)), (int(x_min) + int(width), int(y_min) + int(height)), (255, 0, 0))
 			blur = anno_bbox[4]
 			occlusion = anno_bbox[5]
-			#pose = anno_bbox[9]
-			#if int(invalid) == 1:
-				#label_text_line = label_img_file.readline()
-				#continue
-			if int(width) <30 or int(height)<30:
+			if int(width) < thread_hold or int(height)< thread_hold:
 				label_text_line = label_img_file.readline()
 				continue
 			objects = doc.createElement('objects')
@@ -412,13 +524,16 @@ def generate_xml_from_wider_face(label_source_folder, img_filename, xml_save_fol
 
 
 def main():
+	'''
 	for sub in anno_src_wider_dir:
-		dir = "../wider_face_split/"+sub
+		dir = "../../dataset/facedata/wider_face_split/"+sub
 		load_wider_split(dir)
 	generate_pascal_image_set(root_dir+'wider_face/JPEGImages', root_dir+'wider_face/ImageSets/Main')
 	for file in wider_directory:
-		shuffle_file('../wider_face/ImageSets/Main'+'/'+file+'.txt')
-	# draw_histogram_base_data()
+		shuffle_file('../../dataset/facedata/wider_face/ImageSets/Main'+'/'+file+'.txt')
+	 '''
+	#draw_histogram_base_data()
+	draw_histogram_specfic_range_base_data()
 
 
 if __name__ == '__main__':
