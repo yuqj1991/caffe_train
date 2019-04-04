f7945cdc8a75e11e04629bcbe18872bc9c0bf06e
yuqj1990 <yuqj1990@deepano.com>
2019-04-04 18:44:36 +0800

    ...

--- examples/deepano_face/face_attributes/scripts/deepano_train.sh
+++ examples/deepano_face/face_attributes/scripts/deepano_train.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if ! test -f ../prototxt/facelandmark_train.prototxt ;then
+if ! test -f ../prototxt/facelandmarknet/facelandmark_train.prototxt ;then
 	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-if ! test -f ../prototxt/facelandmark_test.prototxt ;then
+if ! test -f ../prototxt/facelandmarknet/facelandmark_test.prototxt ;then
 	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
