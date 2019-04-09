57a8d959efac003d2bccdfaebd5b5fb58b98691a
yuqj1990 <yuqj@deepano.com>
2019-04-09 21:29:59 +0800

    ...

--- examples/deepano_face/face_detector/scripts/deepano_train_resnet.sh
+++ examples/deepano_face/face_detector/scripts/deepano_train_resnet.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-if ! test -f ../prototxt/deepano_light_face_train.prototxt ;then
+if ! test -f ../prototxt/deepano_light_face_train_resblock_v2.prototxt ;then
 	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-if ! test -f ../prototxt/deepano_light_face_test.prototxt ;then
+if ! test -f ../prototxt/deepano_light_face_test_resblock_v2.prototxt ;then
 	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train --solver=../solver/deepano_solver_train_blur_resnet.prototxt -gpu 0 \
+../../../../build/tools/caffe train --solver=../solver/deepano_solver_train_blur_resnet.prototxt 
+#-gpu 0 \
 #--snapshot=../snapshot/deepanoFace_res_iter_174038.solverstate
