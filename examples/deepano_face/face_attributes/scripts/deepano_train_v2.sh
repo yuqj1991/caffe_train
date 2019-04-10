3098181161b81570ed1edf0deaa089759b958ead
yuqj1990 <yuqj1990@deepano.com>
2019-04-10 16:26:30 +0800

    ...

--- examples/deepano_face/face_attributes/scripts/deepano_train_v2.sh
+++ examples/deepano_face/face_attributes/scripts/deepano_train_v2.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-if ! test -f ../prototxt/facelandmarknet/facelandmark_train.prototxt ;then
+if ! test -f ../prototxt/facelandmarknet/facelandmark_train_v2.prototxt ;then
 	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-if ! test -f ../prototxt/facelandmarknet/facelandmark_test.prototxt ;then
+if ! test -f ../prototxt/facelandmarknet/facelandmark_test_v2.prototxt ;then
 	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train --solver=../solver/facelandmark_solver_train.prototxt -gpu 0 \
+../../../../build/tools/caffe train --solver=../solver/facelandmark_solver_train_v2.prototxt -gpu 0 \
 #--snapshot=../snapshot/deepanoFacelandmark_iter_1087.solverstate
