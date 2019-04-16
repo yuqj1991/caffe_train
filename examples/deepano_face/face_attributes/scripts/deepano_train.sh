6aabdcccff034cd55be5721e94b7c36afa32ce6e
tonny Yu <tonny.yu@honeywell.com>
2019-04-16 18:30:15 +0800

    ...

--- examples/deepano_face/face_attributes/scripts/deepano_train.sh
+++ examples/deepano_face/face_attributes/scripts/deepano_train.sh
@@ -10,4 +10,4 @@ if ! test -f ../prototxt/facelandmarknet/facelandmark_test.prototxt ;then
         exit 1
 fi
 ../../../../build/tools/caffe train --solver=../solver/facelandmark_solver_train.prototxt -gpu 0 \
-#--snapshot=../snapshot/deepanoFacelandmark_iter_17155.solverstate
+--snapshot=../snapshot/deepanoFacelandmark_iter_352891.solverstate
