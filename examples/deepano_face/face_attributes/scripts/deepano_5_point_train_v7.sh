fa605caf92fc09d70f21140345163b254de1eda4
yuqj1990 <yuqj1990@deepano.com>
2019-04-25 15:45:43 +0800

    ....

---
+++ examples/deepano_face/face_attributes/scripts/deepano_5_point_train_v7.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if ! test -f ../prototxt/facelandmarknet/facelandmark_train_v7.prototxt ;then
+	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+if ! test -f ../prototxt/facelandmarknet/facelandmark_test_v7.prototxt ;then
+	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+../../../../build/tools/caffe train --solver=../solver/facelandmark_solver_train_v7.prototxt -gpu 0 \
+#--snapshot=../snapshot/deepanoFacelandmark_v5_iter_400000.solverstate
