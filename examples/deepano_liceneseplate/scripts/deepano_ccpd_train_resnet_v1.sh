f52987a55ad3e2f6bb5c5ee773740e1be18a3f0e
yuqj1990 <yuqj@deepano.com>
2019-04-30 21:46:57 +0800

    ...

---
+++ examples/deepano_liceneseplate/scripts/deepano_ccpd_train_resnet_v1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if ! test -f ../prototxt/deepano_light_ccpd_train_resblock_v1.prototxt ;then
+	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+if ! test -f ../prototxt/deepano_light_ccpd_test_resblock_v1.prototxt ;then
+	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+../../../build/tools/caffe train --solver=../solver/deepano_solver_train_blur_resnet_v1.prototxt -gpu 1 \
+
