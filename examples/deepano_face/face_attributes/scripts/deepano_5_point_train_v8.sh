0d3bf5d090876deaa9b28c7fbefe5a3a6e4c4fe2
yuqj1990 <yuqj@deepano.com>
2019-05-04 11:24:41 +0800

    ...

--- examples/deepano_face/face_attributes/scripts/deepano_5_point_train_v8.sh
+++ examples/deepano_face/face_attributes/scripts/deepano_5_point_train_v8.sh
@@ -9,5 +9,5 @@ if ! test -f ../prototxt/facelandmarknet/facelandmark_test_v8.prototxt ;then
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train --solver=../solver/facelandmark_solver_train_v8.prototxt -gpu 2 \
---snapshot=../snapshot/deepanoFacelandmark_v8_iter_738300.solverstate
+../../../../build/tools/caffe train --solver=../solver/facelandmark_solver_train_v8.prototxt -gpu 3 \
+#--snapshot=../snapshot/deepanoFacelandmark_v8_iter_738300.solverstate
