fe402be5149ac638c9a30d4fd21d7df476fbaf82
tonny Yu <tonny.yu@honeywell.com>
2019-05-09 19:04:56 +0800

    ...

---
+++ examples/deepano_face/face_detector/scripts/deepano_train_resnet_v5.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if ! test -f ../prototxt/deepano_light_face_train_resblock_v5.prototxt ;then
+	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+if ! test -f ../prototxt/deepano_light_face_test_resblock_v5.prototxt ;then
+	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+../../../../build/tools/caffe train --solver=../solver/deepano_solver_train_blur_resnet_v5.prototxt -gpu 2 \
+#--snapshot=../snapshot/deepanoFace_res_v5_iter_76871.solverstate
