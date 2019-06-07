77f01cb474ab3b7288a528dc29397f7c0ab087d9
yuqj1990 <yuqj@deepano.com>
2019-06-07 18:40:37 +0800

    ...

--- examples/deepano_liceneseplate/scripts/deepano_ccpd_rec_v1.sh
+++ examples/deepano_liceneseplate/scripts/deepano_ccpd_rec_v1.sh
@@ -10,4 +10,4 @@ if ! test -f ../prototxt/deepano_light_ccpd_rec_test_v1.prototxt ;then
         exit 1
 fi
 ../../../build/tools/caffe train --solver=../solver/deepano_solver_ccpd_rec_v1.prototxt -gpu 1 \
---snapshot=../snapshot/deepanoccpd_rec_v1_iter_287994.solverstate
+--snapshot=../snapshot/deepanoccpd_rec_v1_iter_1256678.solverstate
