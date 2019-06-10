16bc443c46df30f8cc78807ff34df4086e29e834
yuqj1990 <yuqj@deepano.com>
2019-06-10 14:06:15 +0000

    rename prototxt

--- examples/deepano_face/face_detector/scripts/deepano_train_resnet_v5.sh
+++ examples/deepano_face/face_detector/scripts/deepano_train_resnet_v5.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-if ! test -f ../prototxt/deepano_light_face_train_resblock_v5.prototxt ;then
-	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
+if ! test -f ../prototxt/deepano_face_train_v5.prototxt ;then
+	echo "error: ../prototxt/deepano_face_train_v5.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-if ! test -f ../prototxt/deepano_light_face_test_resblock_v5.prototxt ;then
-	echo "error: ../prototxt/deepano_light_face_train.prototxt does not exit."
+if ! test -f ../prototxt/deepano_face_test_v5.prototxt ;then
+	echo "error: ../prototxt/deepano_face_test_v5.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train --solver=../solver/deepano_solver_train_blur_resnet_v5.prototxt -gpu 0 \
+../../../../build/tools/caffe train --solver=../solver/deepano_solver_train_v5.prototxt -gpu 0 \
 #--snapshot=../snapshot/deepanoFace_res_v5_iter_5000.solverstate
