5300e0691ecc4c87a807923c2386132b2d20f666
yuqj1990 <yuqj1990@deepano.com>
2019-06-24 13:54:12 +0800

    ...

---
+++ examples/deepano_face/face_attributes/scripts/deepano_angle_v2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if ! test -f ../prototxt/faceanglenet/faceanglenet_train_v2.prototxt ;then
+	echo "error: ../prototxt/faceanglenet_train_v2.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+if ! test -f ../prototxt/faceanglenet/faceanglenet_test_v2.prototxt ;then
+	echo "error: ../prototxt/faceanglenet_test_v2.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+../../../../build/tools/caffe train --solver=../solver/faceangle_solver_train_v2.prototxt -gpu 0 \
+#--snapshot=../snapshot/deepanoFaceangle_v1_iter_1280.solverstate
