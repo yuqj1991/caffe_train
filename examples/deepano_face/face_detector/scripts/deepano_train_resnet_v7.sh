8052f9bf769327459832ce17c48225a34359b073
yuqj <yuqj@deepano.com>
2019-07-02 20:30:07 +0800

    ...

--- examples/deepano_face/face_detector/scripts/deepano_train_resnet_v7.sh
+++ examples/deepano_face/face_detector/scripts/deepano_train_resnet_v7.sh
@@ -9,5 +9,5 @@ if ! test -f ../prototxt/deepano_face_test_v7.prototxt ;then
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train --solver=../solver/deepano_solver_train_v7.prototxt -gpu 0 \
+../../../../build/tools/caffe train --solver=../solver/deepano_solver_train_v7.prototxt -gpu 1 \
 #--snapshot=../snapshot/deepanoFace_res_v6_iter_62734.solverstate
