4ba916aee5e98f59585b8e213de69d70c2561f97
yuqj <yuqj@deepano.com>
2019-07-13 17:50:10 +0800

    ...

--- examples/deepano_face/face_detector/scripts/deepano_train_resnet_v5.sh
+++ examples/deepano_face/face_detector/scripts/deepano_train_resnet_v5.sh
@@ -10,4 +10,4 @@ if ! test -f ../prototxt/deepano_face_test_v5.prototxt ;then
         exit 1
 fi
 ../../../../build/tools/caffe train --solver=../solver/deepano_solver_train_v5.prototxt -gpu 1 \
---snapshot=../snapshot/face_detector_v5_iter_43878.solverstate
+#--snapshot=../snapshot/face_detector_v5_iter_43878.solverstate
