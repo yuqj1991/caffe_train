0840a7fa52f8245dc53c6c525f503dd1f93425b1
yuqj1990 <yuqj@deepano.com>
2019-07-18 21:58:20 +0800

    ...

--- examples/face/face_detector/scripts/single_blur_occlu/train_v1.sh
+++ examples/face/face_detector/scripts/single_blur_occlu/train_v1.sh
@@ -9,5 +9,5 @@ if ! test -f ../../prototxt/single_blur_occlu/test_crop.prototxt ;then
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../../build/tools/caffe train --solver=../../solver/solver_blur_occlu/solver_train_v1.prototxt -gpu 0 \
+../../../../../build/tools/caffe train --solver=../../solver/solver_blur_occlu/solver_train_v1.prototxt -gpu 2 \
 #--snapshot=../../snapshot/single_blur_occlu_iter_5898.solverstate
