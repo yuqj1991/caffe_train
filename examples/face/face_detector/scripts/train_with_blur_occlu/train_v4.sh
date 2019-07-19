8992a870f7e93ab0566db81d90927b1200f8ea0e
yuqj1990 <yuqj@deepano.com>
2019-07-19 20:13:39 +0800

    ...

--- examples/face/face_detector/scripts/train_with_blur_occlu/train_v4.sh
+++ examples/face/face_detector/scripts/train_with_blur_occlu/train_v4.sh
@@ -9,5 +9,5 @@ if ! test -f ../../prototxt/detect_with_blur_occlu/face_test_v4.prototxt ;then
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../../build/tools/caffe train --solver=../../solver/solver_with_blur_occlu/solver_train_v4.prototxt -gpu 3 \
+../../../../../build/tools/caffe train --solver=../../solver/solver_with_blur_occlu/solver_train_v4.prototxt -gpu 2 \
 #--snapshot=../../snapshot/face_detector_with_v4_iter_6791.solverstate
