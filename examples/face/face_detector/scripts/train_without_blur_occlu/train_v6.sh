25894986778bfc5d6f3e06f7479d36f466094c32
yuqj <yuqj@deepano.com>
2019-08-08 20:12:57 +0800

    ...

--- examples/face/face_detector/scripts/train_without_blur_occlu/train_v6.sh
+++ examples/face/face_detector/scripts/train_without_blur_occlu/train_v6.sh
@@ -10,4 +10,4 @@ if ! test -f ../../prototxt/detect_without_blur_occlu/face_test_v6.prototxt ;the
         exit 1
 fi
 ../../../../../build/tools/caffe train --solver=../../solver/solver_without_blur_occlu/solver_train_v6.prototxt -gpu 0 \
-#--snapshot=../../snapshot/face_detector_without_v6_iter_5000.solverstate
+--snapshot=../../snapshot/face_detector_without_v6_iter_16688.solverstate
