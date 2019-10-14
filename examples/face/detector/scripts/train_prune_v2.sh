6a1b7b1f9afb009fcd31dea435c6c75e9eaed414
yuqj <yuqj@deepano.com>
2019-10-14 20:48:07 +0800

    ...

---
+++ examples/face/detector/scripts/train_prune_v2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if ! test -f ../prototxt/modelprune/face_train_v2.prototxt ;then
+	echo "error: ../prototxt/modelprune/face_train_v2.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+if ! test -f ../prototxt/modelprune/face_test_v2.prototxt ;then
+	echo "error: ../prototxt/modelprune/face_test_v2.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+../../../../build/tools/caffe train --solver=../solver/solver_train_prune_v2.prototxt -gpu 0 \
+#--snapshot=../../snapshot/face_v8_iter_6791.solverstate
--- src/caffe/layers/multibox_loss_layer.cpp
+++ src/caffe/layers/multibox_loss_layer.cpp
@@ -250,8 +250,13 @@ void MultiBoxSSDLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom
         normalization_, num_, num_priors_, num_matches_);
     top[0]->mutable_cpu_data()[0] += conf_loss_.cpu_data()[0] / normalizer;
   }
-  #if 0
-  LOG(INFO)<<": "<<top[0]->mutable_cpu_data()[0];
+  #if 1
+  Dtype normalizer = LossLayer<Dtype>::GetNormalizer(
+        normalization_, num_, num_priors_, num_matches_);
+  LOG(INFO)<<"loc loss: "<<loc_loss_.cpu_data()[0] / normalizer
+           << "; conf loss: "<< conf_loss_.cpu_data()[0] / normalizer
+           <<"; num_prior_: "<< num_priors_ 
+           << "; num_matches_: "<< num_matches_;
   #endif
 }
 
