067f56789b7481837359d7e9b6e7ab3e82703b3e
yuqj1990 <yuqj@deepano.com>
2019-10-16 13:50:13 +0000

    ...

--- scripts/umdfaces2voc2007/convert.py
+++ scripts/umdfaces2voc2007/convert.py
@@ -6,27 +6,27 @@ import numpy as np
 
 SOURCE_IMG_FILE_FOLDER = '../../../dataset/facedata/umdface/JPEGImages/'
 CROP_IMG_FILE_FOLDER = '../../../dataset/facedata/umdface/annoImages/'
+CROP_IMG_FILE_FOLDER_DRAW = '../../../dataset/facedata/umdface/cropImg/'
 LABEL_FILE_FOLDER = '../../../dataset/facedata/umdface/labels/'
 ImageSetFileForder = '../../../dataset/facedata/umdface/ImageSet/Main/'
-trainFileName = ['umdfaces_batch1_ultraface_new.csv', 'umdfaces_batch2_ultraface_new.csv']
-testFileName = ['umdfaces_batch3_ultraface_new.csv']
-trainSet = [common.ORI_BATCH1, common.ORI_BATCH2]
-testSet = [ common.ORI_BATCH3]
+annoDict = {common.ORI_BATCH1: 'umdfaces_batch1_ultraface_new.csv', common.ORI_BATCH2: 'umdfaces_batch2_ultraface_new.csv', common.ORI_BATCH3: 'umdfaces_batch3_ultraface_new.csv'}
+trainDataSet = [common.ORI_BATCH1, common.ORI_BATCH2]
+testDataSet = [common.ORI_BATCH3]
 maxMargin = 66
 minMargin = 36
-def batch_work(ori, csvFile, setFile):
+def batch_work(ori, setFile):
     setfile_ = open(setFile, 'w')
-    for ii in range(len(ori)):
-        df = common.read_from_file(SOURCE_IMG_FILE_FOLDER+ori[ii]+csvFile[ii])
+    for annofile in ori:
+        df = common.read_from_file(SOURCE_IMG_FILE_FOLDER+ annofile + annoDict[annofile])
         for row in df.iterrows():
             #Extract Important Imformation
             file_name = row[1]['FILE']
             img_file_name_no_jpg = file_name.split('/')[1].split('.jpg')[0]
             label_full_anno_file_name = LABEL_FILE_FOLDER + img_file_name_no_jpg + '.txt'
-            full_path_image_name = SOURCE_IMG_FILE_FOLDER + ori[ii] + file_name
-            if not os.path.exists(CROP_IMG_FILE_FOLDER + ori[ii] + file_name.split('/')[0]):
-                os.mkdir(CROP_IMG_FILE_FOLDER + ori[ii] + file_name.split('/')[0])
-            ang_path_image_name = CROP_IMG_FILE_FOLDER + ori[ii] + file_name.split('.jpg')[0]+'_crop.jpg'
+            full_path_image_name = SOURCE_IMG_FILE_FOLDER + annofile + file_name
+            if not os.path.exists(CROP_IMG_FILE_FOLDER + annofile + file_name.split('/')[0]):
+                os.mkdir(CROP_IMG_FILE_FOLDER + annofile + file_name.split('/')[0])
+            ang_path_image_name = CROP_IMG_FILE_FOLDER + annofile + file_name.split('.jpg')[0]+'_crop.jpg'
             fullImg = os.path.abspath(full_path_image_name) + '\n'
             print('label file: %s, and full_path_img : %s'%(label_full_anno_file_name, full_path_image_name))
             label_file_ = open(label_full_anno_file_name, 'w')
@@ -56,7 +56,8 @@ def batch_work(ori, csvFile, setFile):
             nose_point_y = row[1]['P15Y'] - ymin
             left_mouse_point_y = row[1]['P18Y'] -ymin
             right_mouse_point_y = row[1]['P20Y'] - ymin
-            if 0:
+            cv2.imwrite(ang_path_image_name, cropRoi)
+            if 1:
                 pointSet = []
                 pointSet.append((int(left_eye_point_x), int(left_eye_point_y)))
                 pointSet.append((int(right_eye_point_x), int(right_eye_point_y)))
@@ -65,16 +66,13 @@ def batch_work(ori, csvFile, setFile):
                 pointSet.append((int(right_mouse_point_x), int(right_mouse_point_y)))
                 for ii in range(5):
                     cv2.circle(cropRoi, pointSet[ii], 3, (0,0,213), -1)
-                cv2.imshow("face", cropRoi)
-                k = cv2.waitKey(0)
-                break
-            cv2.imwrite(ang_path_image_name, cropRoi)
+                ang_path_image_name_new = CROP_IMG_FILE_FOLDER_DRAW + img_file_name_no_jpg + '_crop.jpg'
+                cv2.imwrite(ang_path_image_name_new, cropRoi)
 
             setfile_.writelines(os.path.abspath(ang_path_image_name) + '\n')
             content = str(left_eye_point_x) + ' ' + str(right_eye_point_x) + ' ' + str(nose_point_x) + ' ' + str(left_mouse_point_x) + ' ' + str(right_mouse_point_x) + ' ' + str(left_eye_point_y) + ' ' + str(right_eye_point_y) + ' ' + str(nose_point_y) + ' ' + str(left_mouse_point_y) + ' ' + str(right_mouse_point_y) + ' ' + str(yaw) + ' ' + str(pitch) + ' ' + str(roll) + ' ' + str(pr_female) + ' ' + str(pr_male) + ' ' + str(boolGlass) + '\n'
             label_file_.write(content)
             label_file_.close()
-        #break
     setfile_.close()
 
 
@@ -83,7 +81,7 @@ if __name__ == '__main__':
     start_time = datetime.datetime.now()
     trainset = ImageSetFileForder + 'training_umdface_pose.txt'
     testset = ImageSetFileForder + 'testing_umdface_pose.txt'
-    batch_work(trainSet, trainFileName, trainset)
-    batch_work(testSet, testFileName, testset)
+    batch_work(trainDataSet, trainset)
+    batch_work(testDataSet, testset)
     end_time = datetime.datetime.now()
     LOG.info('Cost %d Seconds!'%((end_time-start_time).seconds))
