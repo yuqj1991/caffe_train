925e94f477fc324fc57b304e2f3fbb83dfe913a7
yuqj1990 <yuqj@deepano.com>
2019-10-17 18:05:51 +0000

    ...

--- examples/face/attributes/scripts/create_list_umdface.py
+++ examples/face/attributes/scripts/create_list_umdface.py
@@ -3,7 +3,7 @@ import sys
 import os
 import random
 wider_directory = ['training_umdface_pose','testing_umdface_pose']
-root_dir ='../../../../../caffe_train/examples/face/face_attributes/scripts/'
+root_dir ='../../../../../caffe_train/examples/face/attributes/scripts/'
 root_dataset = '../../../../../dataset/facedata/umdface/labels/'
 def shuffle_file(filename):
 	f = open(filename, 'r+')
--- examples/face/detector/scripts/create_data_anno.sh
+++ examples/face/detector/scripts/create_data_anno.sh
@@ -4,7 +4,7 @@ cd $root_dir/scripts
 redo=1
 data_root_dir="../../dataset/facedata"
 dataset_name="wider_face"
-mapfile="../examples/face/face_detector/labelmap_face.prototxt"
+mapfile="../examples/face/detector/labelmap_face.prototxt"
 anno_type="detection"
 db="lmdb"
 min_dim=0
@@ -17,7 +17,7 @@ if [ $redo ]
 then
   extra_cmd="$extra_cmd --redo"
 fi
-for subset in train val
+for subset in wider_train wider_val
 do
-  python create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir ../examples/face/face_detector/scripts/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_"$db $data_root_dir/$dataset_name
+  python create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir ../examples/face/detector/scripts/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_"$db $data_root_dir/$dataset_name
 done
