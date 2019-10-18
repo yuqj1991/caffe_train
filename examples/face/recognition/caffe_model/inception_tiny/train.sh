5be4cb326f63dc802772f6d10431eb61dbbfd71a
yuqj1990 <yuqj@deepano.com>
2019-10-18 20:32:53 +0800

    ...

---
+++ examples/face/recognition/caffe_model/inception_tiny/train.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if ! test -f inception_resnet_v2_tiny_train.prototxt ;then
+	echo "error: inception_resnet_v2_tiny_train.prototxt does not exit."
+	echo "please generate your own model prototxt primarily."
+        exit 1
+fi
+../../../../../build/tools/caffe train --solver=inception_resnet_v2_tiny_solver.prototxt -gpu 2 \
+#--snapshot=../snapshot/face_iter_5000.solverstate
