d1cdddb0a6b74e869505b594ae86f84ac7a98ce4
yuqj <yuqj@deepano.com>
2019-10-19 18:11:27 +0800

    ...

--- examples/face/detector/scripts/train_v5.sh
+++ examples/face/detector/scripts/train_v5.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-if ! test -f ../../prototxt/modelfull/face_train_v5.prototxt ;then
+if ! test -f ../prototxt/modelfull/face_train_v5.prototxt ;then
 	echo "error: ../../prototxt/modelfull/face_train_v5.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-if ! test -f ../../prototxt/modelfull/face_test_v5.prototxt ;then
+if ! test -f ../prototxt/modelfull/face_test_v5.prototxt ;then
 	echo "error: ../../prototxt/modelfull/face_test_v5.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../../build/tools/caffe train --solver=../../solver/solver_without_blur_occlu/solver_train_v5.prototxt -gpu 0 \
+../../../../build/tools/caffe train --solver=../solver/solver_train_v5.prototxt -gpu 0 \
 #--snapshot=../../snapshot/face_v5_iter_5000.solverstate
