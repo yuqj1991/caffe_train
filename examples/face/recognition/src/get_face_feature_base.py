781918da1824261e4866e143574031afaf1bde0a
tonny Yu <tonny.yu@honeywell.com>
2019-10-24 16:22:59 +0800

    ...

---
+++ examples/face/recognition/src/get_face_feature_base.py
@@ -0,0 +1,77 @@
+# coding=utf-8
+"""Performs face detection in realtime.
+
+Based on code from https://github.com/shanren7/real_time_face_recognition
+"""
+# MIT License
+#
+# Copyright (c) 2017 François Gervais
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import argparse
+import sys
+import time
+import os
+import cv2
+
+import face
+
+
+def add_overlays(frame, faces, frame_rate):
+    if faces is not None:
+        for face in faces:
+            face_bb = face.bounding_box.astype(int)
+            cv2.rectangle(frame,
+                          (face_bb[0], face_bb[1]), (face_bb[2], face_bb[3]),
+                          (0, 255, 0), 2)
+            if face.name is not None:
+                cv2.putText(frame, face.name, (face_bb[0], face_bb[3]),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),
+                            thickness=2, lineType=2)
+
+    cv2.putText(frame, str(frame_rate) + " fps", (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),
+                thickness=2, lineType=2)
+
+
+def main(args):
+    face_recognition = face.Recognition()
+    start_time = time.time()
+    for img_file in os.listdir(args.facedir):
+        filepath = os.path.abspath(args.facedir + '/' + img_file)
+        frame = cv2.imread(filepath)
+        face_embedding = face_recognition.faceencoder(frame)
+        embedding_file = open(img_file.split('.jpg')[0], 'w')
+        if len(face_embedding) > 1:
+            raise Exception("检测到的人数过多")
+        if len(face_embedding) == 1:
+            embedding_file.write(face_embedding[0])
+        embedding_file.close()
+
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--facedir', type = str,
+                        help='Enable some debug outputs.')
+    return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
