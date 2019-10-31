d6d04ca9bbc319ef8b1d9a336321ef17b25a978e
yuqj1990 <yuqj@deepano.com>
2019-10-31 12:31:00 +0000

    ...

--- examples/face/attributes/scripts/face_attri_train.sh
+++ examples/face/attributes/scripts/face_attri_train.sh
@@ -10,8 +10,4 @@ if ! test -f ../prototxt/faceattrinet/test.prototxt ;then
         exit 1
 fi
 ../../../../build/tools/caffe train --solver=../solver/faceattri_solver_train.prototxt -gpu 0 \
-<<<<<<< HEAD
 #--snapshot=../snapshot/faceattri_iter_47290.solverstate
-=======
-#--snapshot=../snapshot/faceattri_iter_158573.solverstate
->>>>>>> 7b089c81027a0358284eabaf080babab0c3696d3
