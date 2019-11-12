f39e4887f1d9081aa2024f4e6348d7dda173e875
tonny Yu <tonny.yu@honeywell.com>
2019-11-12 12:06:13 +0800

    ...

--- examples/face/detector/scripts/train_prune_v3.sh
+++ examples/face/detector/scripts/train_prune_v3.sh
@@ -9,5 +9,5 @@ if ! test -f ../prototxt/modelprune/face_test_v3.prototxt ;then
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train --solver=../solver/solver_train_v3.prototxt -gpu 1 \
+../../../../build/tools/caffe train --solver=../solver/solver_train_prune_v3.prototxt -gpu 1 \
 #--snapshot=../snapshot/face_v3_iter_44807.solverstate
