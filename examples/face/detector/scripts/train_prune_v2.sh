c4fa5b53932f17e7af688bb70e8a11676bf46f32
tonny Yu <tonny.yu@honeywell.com>
2019-11-12 13:55:48 +0800

    ...

--- examples/face/detector/scripts/train_prune_v2.sh
+++ examples/face/detector/scripts/train_prune_v2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-if ! test -f ../prototxt/modelprune/face_train_v2.prototxt ;then
-	echo "error: ../prototxt/modelprune/face_train_v2.prototxt does not exit."
+if ! test -f ../prototxt/model/face_train_v2.prototxt ;then
+	echo "error: ../prototxt/model/face_train_v2.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-if ! test -f ../prototxt/modelprune/face_test_v2.prototxt ;then
-	echo "error: ../prototxt/modelprune/face_test_v2.prototxt does not exit."
+if ! test -f ../prototxt/model/face_test_v2.prototxt ;then
+	echo "error: ../prototxt/model/face_test_v2.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
--- examples/face/detector/scripts/train_v5.sh
+++ examples/face/detector/scripts/train_v5.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-if ! test -f ../prototxt/modelfull/face_train_v5.prototxt ;then
-	echo "error: ../../prototxt/modelfull/face_train_v5.prototxt does not exit."
+if ! test -f ../prototxt/model/face_train_v5.prototxt ;then
+	echo "error: ../../prototxt/model/face_train_v5.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-if ! test -f ../prototxt/modelfull/face_test_v5.prototxt ;then
-	echo "error: ../../prototxt/modelfull/face_test_v5.prototxt does not exit."
+if ! test -f ../prototxt/model/face_test_v5.prototxt ;then
+	echo "error: ../../prototxt/model/face_test_v5.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
