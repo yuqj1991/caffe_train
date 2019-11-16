ccfbf298a34a845aa2e16f47fd6cc3d0e7267d01
yuqj1990 <yuqj@deepano.com>
2019-11-16 11:11:05 +0800

    ...

--- examples/face/recognition/caffe/train_v4.sh
+++ examples/face/recognition/caffe/train_v4.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if ! test -f ./prototxt/resnet_18/train.prototxt ;then
+if ! test -f ./prototxt/model/train.prototxt ;then
 	echo "error: train.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train --solver=./prototxt/resnet_18/solver.prototxt -gpu 0 \
+../../../../build/tools/caffe train --solver=./prototxt/model/solver.prototxt -gpu 2 \
 #--snapshot=./face_recog/tiny_iter_148441.solverstate
