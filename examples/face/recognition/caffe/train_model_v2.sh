12bea181267b4972e41abde006bef88a20890d9e
tonny Yu <tonny.yu@honeywell.com>
2019-11-18 16:58:02 +0800

    ...

--- examples/face/recognition/caffe/train_model_v2.sh
+++ examples/face/recognition/caffe/train_model_v2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if ! test -f ./prototxt/resnet/train.prototxt ;then
+if ! test -f ./prototxt/model/train_v2.prototxt ;then
 	echo "error: train.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train --solver=./prototxt/resnet/solver.prototxt -gpu 0 \
+../../../../build/tools/caffe train --solver=./prototxt/model/train_v2.prototxt -gpu 0 \
 #--snapshot=./face_recog/tiny_iter_148441.solverstate
