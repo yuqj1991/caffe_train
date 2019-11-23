6ef1581fb9430e26cb3fc92dfa62f16af65115f3
yuqj1990 <yuqj@deepano.com>
2019-11-23 11:02:54 +0800

    ...

--- scripts/generate_ccpd_lpnum.py
+++ scripts/generate_ccpd_lpnum.py
@@ -11,7 +11,7 @@ import random
 import matplotlib.pyplot as plot
 
 root_dir = "../../dataset/car_person_data/car_license/ccpd_dataset"
-ccpd_anno_img_dir = "/home/deep/workspace/dataset/car_person_data/car_license/ccpd_dataset/annoImg"
+ccpd_anno_img_dir = "../../dataset/car_person_data/car_license/ccpd_dataset/annoImg"
 if not os.path.exists(ccpd_anno_img_dir):
 	os.makedirs(ccpd_anno_img_dir)
 set_dir = "ImageSets/Main"
@@ -20,12 +20,31 @@ label_dir = "label"
 lengthTestset = 50000
 lengthTrain = 280001
 provNum, alphaNum, adNum = 34, 25, 35
-provinces = ["皖", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "苏", "浙", "京", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤", "桂",
-             "琼", "川", "贵", "云", "藏", "陕", "甘", "青", "宁", "新", "警", "学", "O"]
-alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-             'X', 'Y', 'Z', 'O']
-ads = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-       'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'O']
+provinces = ["皖", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "苏", "浙", "京", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤", "桂", "琼", "川", "贵", "云", "藏", "陕", "甘", "青", "宁", "新", "警", "学", "O"]
+alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'O']
+ads = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'O']
+
+
+index = {"京": 0, "沪": 1, "津": 2, "渝": 3, "冀": 4, "晋": 5, "蒙": 6, "辽": 7, "吉": 8, "黑": 9, "苏": 10, "浙": 11, "皖": 12, "闽": 13, "赣": 14, "鲁": 15, "豫": 16, "鄂": 17, "湘": 18, "粤": 19, "桂": 20, "琼": 21, "川": 22, "贵": 23, "云": 24, "藏": 25, "陕": 26, "甘": 27, "青": 28, "宁": 29, "新": 30, "0": 31, "1": 32, "2": 33, "3": 34, "4": 35, "5": 36, "6": 37, "7": 38, "8": 39, "9": 40, "A": 41, "B": 42, "C": 43, "D": 44, "E": 45, "F": 46, "G": 47, "H": 48, "J": 49, "K": 50, "L": 51, "M": 52, "N": 53, "P": 54, "Q": 55, "R": 56, "S": 57, "T": 58, "U": 59, "V": 60, "W": 61, "X": 62, "Y": 63, "Z": 64}
+
+
+def get_label(name):
+	lpr_str = name.split('_')
+	label = []
+	for s in lpr_str:
+		label.append(index[s])
+	return np.array(label)
+
+
+def convertIndex2label(licenseNum):
+	fist = provinces[licenseNum[0]]
+	second = alphabets[licenseNum[1]]
+	third = ads[licenseNum[2]]
+	forth = ads[licenseNum[3]]
+	fifth = ads[licenseNum[4]]
+	sixth = ads[licenseNum[5]]
+	seventh = ads[licenseNum[6]]
+	return fist + "_" + second + "_" + third + "_" + forth + "_" + fifth + "_" + sixth + "_" + seventh
 
 
 def generate_label(imagefilepath, savefilepath):
@@ -55,8 +74,42 @@ def generate_label(imagefilepath, savefilepath):
 	label_file_ = open(savefilepath, "w")
 	label_file_.write(labelContent)
 	label_file_.close()
-	
-	
+
+
+def generate_anothor_label(imagefilepath, savefilepath):
+	print(imagefilepath)
+	img = cv2.imread(imagefilepath)
+	img_h = img.shape[0]
+	img_w = img.shape[1]
+	labelInfo = imagefilepath.split("/")[-1].split(".jpg")[0].split("-")
+	labelbndBox = labelInfo[2].split('_')
+	left_upBox_x = labelbndBox[0].split("&")[0]
+	left_upBox_y = labelbndBox[0].split("&")[1]
+	right_bottom_x = labelbndBox[1].split("&")[0]
+	right_bottom_y = labelbndBox[1].split("&")[1]
+	cropped = img[(int(left_upBox_y) - 22):(int(right_bottom_y)+22), (int(left_upBox_x) -22):(int(right_bottom_x) + 22), :]
+	cv2.imwrite(ccpd_anno_img_dir+"/"+"crop_"+str(imagefilepath.split("/")[-1]), cropped)
+	exactbndBox = labelInfo[3].split('_')
+	vertices_1_x = exactbndBox[0].split("&")[0]
+	vertices_1_y = exactbndBox[0].split("&")[1]
+	vertices_2_x = exactbndBox[1].split("&")[0]
+	vertices_2_y = exactbndBox[1].split("&")[1]
+	vertices_3_x = exactbndBox[2].split("&")[0]
+	vertices_3_y = exactbndBox[2].split("&")[1]
+	vertices_4_x = exactbndBox[3].split("&")[0]
+	vertices_4_y = exactbndBox[3].split("&")[1]
+	licenseNum = labelInfo[4].split('_')
+	license = []
+	for idnum in licenseNum:
+		license.append(int(idnum))
+	numberString = convertIndex2label(license)
+	numbers = get_label(numberString)
+	labelContent =" ".join(str(b) for b in numbers)
+	label_file_ = open(savefilepath, "w")
+	label_file_.write(labelContent)
+	label_file_.close()
+
+
 def generate_train_setfile(imagefiledir, setfile):
 	setfile_ = open(setfile, "a+")
 	global lengthTrain
@@ -66,10 +119,11 @@ def generate_train_setfile(imagefiledir, setfile):
 		annopath = ccpd_anno_img_dir+"/"+"crop_"+str(absimgfilepath.split("/")[-1])
 		setfile_.write(annopath+'\n')
 		savefilepath = root_dir+'/'+label_dir +'/crop_'+ absimgfilepath.split('/')[-1].split('.jpg')[0]
-		generate_label(absimgfilepath, savefilepath)
+		#generate_label(absimgfilepath, savefilepath)
+		generate_anothor_label(absimgfilepath, savefilepath)
 		lengthTrain += 1
-	
-	
+
+
 def split_train_setfile(trainSetfilepath, testSetfilepath, testline, count, setfile):
 	lines = []
 	with open(trainSetfilepath, 'r+w') as trainfile:
@@ -88,8 +142,8 @@ def split_train_setfile(trainSetfilepath, testSetfilepath, testline, count, setf
 	for linecontent in lines:
 		file_.write(linecontent)
 	file_.close()
-	
-	
+
+
 def generate_random_test_indexlist(lengthTrainset, lengthTestset):
 	test_length = 1
 	test_list = []
