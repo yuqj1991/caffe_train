b34ae204983cdda89c01011757c11d1b5874dd5c
yuqj1990 <yuqj@deepano.com>
2019-11-25 07:19:15 +0000

    ...

--- examples/face/detector/scripts/train_prune_320_v1.sh
+++ examples/face/detector/scripts/train_prune_320_v1.sh
@@ -10,6 +10,6 @@ if ! test -f ../prototxt/Prune_320x320/test_v1.prototxt ;then
         exit 1
 fi
 ../../../../build/tools/caffe train --solver=../prototxt/Prune_320x320/solver_v1.prototxt -gpu 0 \
---weights ../snapshot/face_iter_80000.caffemodel
+#--weights ../snapshot/face_iter_80000.caffemodel
 #--snapshot=../snapshot/face_iter_100000.solverstate
 
