eefdc229d5b74dee677ac1faae9fba4b74c963fc
tonny Yu <tonny.yu@honeywell.com>
2019-12-25 09:41:14 +0800

    ...

--- examples/reID/caffe/train_google.sh
+++ examples/reID/caffe/train_google.sh
@@ -2,5 +2,5 @@
 model_dir=models/market1501/googlenet
 pre_train_dir=${HOME}/datasets/model_pretrained/googlenet
 
-../../../build/tools/caffe train --solver ./prototxt/googlenet/solver.prototxt -gpu 0 \
+../../../build/tools/caffe train --solver ./prototxt/googlenet/solver.prototxt -gpu 1 \
 #--snapshot=../snapshot/reid_iter_6824.solverstate
