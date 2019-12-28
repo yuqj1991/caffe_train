7ef72b3508a15a028405aef7622367015b37be3c
yuqj1990 <yuqj@deepano.com>
2019-12-28 05:10:38 +0000

    ...

--- examples/reID/caffe/train_google.sh
+++ examples/reID/caffe/train_google.sh
@@ -2,5 +2,5 @@
 model_dir=models/market1501/googlenet
 pre_train_dir=${HOME}/datasets/model_pretrained/googlenet
 
-../../../build/tools/caffe train --solver ./prototxt/Googlenet/solver.prototxt -gpu 1 \
+../../../build/tools/caffe train --solver ./prototxt/Googlenet/solver.prototxt -gpu 0 \
 #--snapshot=../snapshot/reid_iter_6824.solverstate
