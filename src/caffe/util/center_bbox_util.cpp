71f8fd95e34198168084e3324f1bb15e4428fe26
yuqj1990 <yuqj@deepano.com>
2020-02-18 05:58:46 +0000

    ...

--- src/caffe/util/center_bbox_util.cpp
+++ src/caffe/util/center_bbox_util.cpp
@@ -15,8 +15,6 @@
 
 int count_gt = 0;
 int count_one = 0;
-int count_zero = 0;
-int count_no_zero = 0;
 
 namespace caffe {
 template<typename Dtype>
@@ -24,21 +22,21 @@ Dtype gaussian_radius(const Dtype heatmap_width, const Dtype heatmap_height, con
 
   Dtype a1  = Dtype(1.0);
   Dtype b1  = (heatmap_width + heatmap_height);
-  Dtype c1  = heatmap_width * heatmap_height * (1 - min_overlap) / (1 + min_overlap);
+  Dtype c1  = Dtype( heatmap_width * heatmap_height * (1 - min_overlap) / (1 + min_overlap));
   Dtype sq1 = std::sqrt(b1 * b1 - 4 * a1 * c1);
-  Dtype r1  = Dtype(b1 + sq1) / 2;
+  Dtype r1  = Dtype((b1 + sq1) / 2);
 
   Dtype a2  = Dtype(4.0);
   Dtype b2  = 2 * (heatmap_height + heatmap_width);
   Dtype c2  = (1 - min_overlap) * heatmap_width * heatmap_height;
   Dtype sq2 = std::sqrt(b2 * b2 - 4 * a2 * c2);
-  Dtype r2  = Dtype(b2 + sq2) / 2;
+  Dtype r2  = Dtype((b2 + sq2) / 2);
 
   Dtype a3  = Dtype(4 * min_overlap);
   Dtype b3  = -2 * min_overlap * (heatmap_height + heatmap_width);
   Dtype c3  = (min_overlap - 1) * heatmap_width * heatmap_height;
   Dtype sq3 = std::sqrt(b3 * b3 - 4 * a3 * c3);
-  Dtype r3  = Dtype(b3 + sq3) / 2;
+  Dtype r3  = Dtype((b3 + sq3) / 2);
   return std::min(std::min(r1, r2), r3);
 }
 
@@ -290,8 +288,6 @@ void GenerateBatchHeatmap(std::map<int, vector<NormalizedBBox> > all_gt_bboxes, 
                               const int num_classes_, const int output_width, const int output_height){
   std::map<int, vector<NormalizedBBox> > ::iterator iter;
   count_gt = 0;
-  count_zero = 0;
-  count_no_zero = 0;
   count_one = 0;
   for(iter = all_gt_bboxes.begin(); iter != all_gt_bboxes.end(); iter++){
     int batch_id = iter->first;
@@ -326,23 +322,10 @@ void GenerateBatchHeatmap(std::map<int, vector<NormalizedBBox> > all_gt_bboxes, 
   for(iter = all_gt_bboxes.begin(); iter != all_gt_bboxes.end(); iter++){
     int batch_id = iter->first;
     vector<NormalizedBBox> gt_bboxes = iter->second;
-    /*for(unsigned ii = 0; ii < gt_bboxes.size(); ii++){
-      const Dtype xmin = gt_bboxes[ii].xmin() * output_width;
-      const Dtype ymin = gt_bboxes[ii].ymin() * output_height;
-      const Dtype xmax = gt_bboxes[ii].xmax() * output_width;
-      const Dtype ymax = gt_bboxes[ii].ymax() * output_height;
-      int center_x = int( (xmin + xmax) / 2 );
-      int center_y = int( (ymin + ymax) / 2 );
-      LOG(INFO)<<"gt_bboxes center_x: "<< center_x << ", gt_bboxes center_y; "<< center_y;
-    }*/
     for(int c = 0; c < num_classes_; c++){
       for(int h = 0 ; h < output_height; h++){
         for(int w = 0; w < output_width; w++){
           int index = batch_id * num_classes_ * output_height * output_width + c * output_height * output_width + h * output_width + w;  
-          if(gt_heatmap[index] == 0.f)
-            count_zero++;
-          if(gt_heatmap[index] != 0.f)
-            count_no_zero++;
           if(gt_heatmap[index] == 1.f){
             count_one++;
             //LOG(INFO)<<"heatmap center_x: "<< w << ", heatmap center_y; "<< h << ", value: "<<gt_heatmap[index];
