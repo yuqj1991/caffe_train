de2f261b5964195425ce501be8ab2f3788441b04
yuqj <yuqj@deepano.com>
2020-03-02 15:34:13 +0800

    ...

--- src/caffe/layers/smooth_L1_loss_layer.cu
+++ src/caffe/layers/smooth_L1_loss_layer.cu
@@ -44,14 +44,6 @@ void SmoothL1LossLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
         diff_.gpu_data(),
         diff_.mutable_gpu_data());  // d := w * (b0 - b1)
   }
-  #if 0
-  const Dtype* in = diff_.gpu_data();
-  for(int i =0; i<count; i++){
-    Dtype val = in[i];
-    Dtype abs_val = abs(val);
-    LOG(INFO)<<"abs_val: "<<abs_val;
-  }
-  #endif
   // NOLINT_NEXT_LINE(whitespace/operators)
   SmoothL1Forward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
       count, diff_.gpu_data(), errors_.mutable_gpu_data());
@@ -60,10 +52,8 @@ void SmoothL1LossLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   Dtype loss;
   caffe_gpu_asum(count, errors_.gpu_data(), &loss);
   top[0]->mutable_cpu_data()[0] = loss / bottom[0]->num();
-  #if 0
-  LOG(INFO)<<"loc loss sum: "<<loss;
-  LOG(INFO)<<"SMOOTH L1 count: "<<count<<" has_weight_: "<<has_weights_
-           <<" bottom[0]->num(): "<< bottom[0]->num();
+  #if 1
+  printf(" bottom[0]->num(): %d", bottom[0]->num());
   #endif
 }
 
