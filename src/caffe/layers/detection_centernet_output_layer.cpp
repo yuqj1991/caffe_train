ec0206b3d83053512f6359ffe7aa5849b7992d5c
yuqj <yuqj@deepano.com>
2020-03-02 09:31:34 +0800

    “....

--- src/caffe/layers/detection_centernet_output_layer.cpp
+++ src/caffe/layers/detection_centernet_output_layer.cpp
@@ -111,7 +111,7 @@ void CenternetDetectionOutputLayer<Dtype>::Forward_cpu(
         top_data[count * 7 + 6] = result_temp[j].ymax;
         //LOG(INFO)<< result_temp[j].xmin <<", "<<result_temp[j].ymin <<", "<<result_temp[j].xmax<<", "<<result_temp[j].ymax;
         LOG(INFO)<<top_data[count * 7 + 1]<<", "<<top_data[count * 7 + 2]<<", "<<top_data[count * 7 + 3]<<
-                  <<", "<<top_data[count * 7 + 4]<<", "<<top_data[count * 7 + 5]<<", "<<top_data[count * 7 + 6];
+                  ", "<<top_data[count * 7 + 4]<<", "<<top_data[count * 7 + 5]<<", "<<top_data[count * 7 + 6];
         ++count;
       }
     }
