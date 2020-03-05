6df040948050544e20940804c33e536cd96e61a3
tonny Yu <tonny.yu@honeywell.com>
2020-03-05 13:52:27 +0800

    ...

--- include/caffe/util/center_bbox_util.hpp
+++ include/caffe/util/center_bbox_util.hpp
@@ -50,7 +50,7 @@ template <typename Dtype>
 void _nms_heatmap(const Dtype* conf_data, Dtype* keep_max_data, const int output_height
                   , const int output_width, const int channels, const int num_batch);
 
-void nms(std::vector<CenterNetInfo>& input, std::vector<CenterNetInfo>& output, float nmsthreshold = 0.3,int type=NMS_MIN);
+void nms(std::vector<CenterNetInfo>& input, std::vector<CenterNetInfo>* output, float nmsthreshold = 0.3,int type=NMS_MIN);
 
 template <typename Dtype>
 void get_topK(const Dtype* keep_max_data, const Dtype* loc_data, const int output_height
