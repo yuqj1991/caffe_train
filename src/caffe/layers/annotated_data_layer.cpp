4800f3e1fee583fe8ab3e8538b3e89cf75bc6184
tonny Yu <tonny.yu@honeywell.com>
2020-03-23 19:30:03 +0800

    ...

--- src/caffe/layers/annotated_data_layer.cpp
+++ src/caffe/layers/annotated_data_layer.cpp
@@ -208,10 +208,13 @@ void AnnotatedDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
     AnnotatedDatum* sampled_datum = NULL;
     bool has_sampled = false;
     if(crop_type_ == AnnotatedDataParameter_CROP_TYPE_CROP_BATCH){
-      if (batch_samplers_.size() > 0) {
+      if (batch_samplers_.size() > 0 || YoloFormat_) {
         // Generate sampled bboxes from expand_datum.
         vector<NormalizedBBox> sampled_bboxes;
-        GenerateBatchSamples(*expand_datum, batch_samplers_, &sampled_bboxes);
+        if(YoloFormat_)
+          GenerateJitterSamples(0.3, &sampled_bboxes);
+        else
+          GenerateBatchSamples(*expand_datum, batch_samplers_, &sampled_bboxes);
         if (sampled_bboxes.size() > 0) {
           // Randomly pick a sampled bbox and crop the expand_datum.
           int rand_idx = caffe_rng_rand() % sampled_bboxes.size();
