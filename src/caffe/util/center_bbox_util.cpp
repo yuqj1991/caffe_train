2cb644350edc5b38fb9b7a691da80c69bceff138
tonny Yu <tonny.yu@honeywell.com>
2020-03-26 19:17:18 +0800

    ...

--- src/caffe/util/center_bbox_util.cpp
+++ src/caffe/util/center_bbox_util.cpp
@@ -1268,7 +1268,7 @@ Dtype EncodeCenterGridObjectSoftMaxLoss(const int batch_size, const int num_chan
   *count_postive = postive;
   *loc_loss_value = loc_loss;
 
-  #if 1
+  #if 0
   Dtype test_class_label[2 * 2 * 2] = {0., 0.5, 0.5, 1., 0.5, 0., 1., 1.};
   Dtype test_pred_data[2 * (2 * 2 * 2)] = {
 
