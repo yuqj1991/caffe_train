96313b7ac4fc78fc6bce65ade2e96bc0a5e4a705
tonny Yu <tonny.yu@honeywell.com>
2020-03-26 14:39:29 +0800

    ...

--- src/caffe/util/center_bbox_util.cpp
+++ src/caffe/util/center_bbox_util.cpp
@@ -1246,7 +1246,7 @@ Dtype EncodeCenterGridObjectSoftMaxLoss(const int batch_size, const int num_chan
       const int gt_bbox_height = static_cast<int>((ymax - ymin) * downRatio);
       int large_side = std::max(gt_bbox_height, gt_bbox_width);
       if(large_side >= loc_truth_scale.first && large_side < loc_truth_scale.second){
-        int RF_xmin = static_cast<int>(xmin  - anchor_scale/(2 * downRatio));
+        /*int RF_xmin = static_cast<int>(xmin  - anchor_scale/(2 * downRatio));
         int RF_xmax = static_cast<int>(xmax  + anchor_scale/(2 * downRatio));
         int RF_ymin = static_cast<int>(ymin  - anchor_scale/(2 * downRatio));
         int RF_ymax = static_cast<int>(ymax  + anchor_scale/(2 * downRatio));
@@ -1294,7 +1294,7 @@ Dtype EncodeCenterGridObjectSoftMaxLoss(const int batch_size, const int num_chan
               class_label[class_index] = 0.5;
             }
           }
-        }
+        }*/
         for(int h = static_cast<int>(ymin); h < static_cast<int>(ymax); h++){
           for(int w = static_cast<int>(xmin); w < static_cast<int>(xmax); w++){
             if(w + (anchor_scale/downRatio) / 2 >= output_width - 1)
@@ -1353,8 +1353,7 @@ Dtype EncodeCenterGridObjectSoftMaxLoss(const int batch_size, const int num_chan
     postive += count;
   }
   // 计算softMax loss value 
-  //SelectHardSample(class_label, channel_pred_data, 3, postive_batch, output_height, 
-  //                                                    output_width, num_channels, batch_size);
+  SelectHardSample(class_label, channel_pred_data, 3, postive_batch, output_height, output_width, num_channels, batch_size);
   score_loss = SoftmaxLossEntropy(class_label, channel_pred_data, batch_size, output_height,
                         output_width, bottom_diff, num_channels);
   *count_postive = postive;
