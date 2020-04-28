5d2782f188b844fa83c5be867b448d27fcb9808f
tonny Yu <tonny.yu@honeywell.com>
2020-04-28 09:48:54 +0800

    ...

--- examples/face/detector/scripts/train_centerGridSoftmax_face_v2.sh
+++ examples/face/detector/scripts/train_centerGridSoftmax_face_v2.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-if ! test -f ../prototxt/Full_640x640/CenterGridSoftmax_face_train_v2.prototxt ;then
-	echo "error: ../prototxt/Full_640x640/CenterGrid_face_train_v2.prototxt does not exit."
+if ! test -f ../prototxt/Full_640x640/CenterGridSoftmax_face_v2_train.prototxt ;then
+	echo "error: ../prototxt/Full_640x640/CenterGridSoftmax_face_v2_train.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-if ! test -f ../prototxt/Full_640x640/CenterGridSoftmax_face_test_v2.prototxt ;then
-	echo "error: ../prototxt/Full_640x640/CenterGrid_face_test_v2.prototxt does not exit."
+if ! test -f ../prototxt/Full_640x640/CenterGridSoftmax_face_v2_test.prototxt ;then
+	echo "error: ../prototxt/Full_640x640/CenterGridSoftmax_face_v2_test.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train --solver=../prototxt/Full_640x640/CenterGridSoftmax_face_solver_v2.prototxt -gpu 0 \
+../../../../build/tools/caffe train --solver=../prototxt/Full_640x640/CenterGridSoftmax_face_v2_solver.prototxt -gpu 0 \
 #--snapshot=../snapshot/CenterGridSoftmax_face_iter_5000.solverstate
