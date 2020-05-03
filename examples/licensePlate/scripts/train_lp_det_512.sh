b09784b78c251065b613141d77cb8acaac48d000
yuqj <yuqj@deepano.com>
2020-05-03 11:07:01 +0800

    ...

--- examples/licensePlate/scripts/train_lp_det_512.sh
+++ examples/licensePlate/scripts/train_lp_det_512.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-if ! test -f ../prototxt/SSD_512X512/MobileNetSSD_train.prototxt ;then
-	echo "error: ../prototxt/SSD_512X512/MobileNetSSD_train.prototxt does not exit."
+if ! test -f ../prototxt/SSD_512x512/MobileNetSSD_train.prototxt ;then
+	echo "error: ../prototxt/SSD_512x512/MobileNetSSD_train.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
 if ! test -f ../prototxt/SSD_512x512/MobileNetSSD_test.prototxt ;then
-	echo "error: ../prototxt/SSD_512X512/MobileNetSSD_test.prototxt does not exit."
+	echo "error: ../prototxt/SSD_512x512/MobileNetSSD_test.prototxt does not exit."
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../build/tools/caffe train --solver=../prototxt/SSD_512X512/solver_train.prototxt -gpu 0 \
+../../../build/tools/caffe train --solver=../prototxt/SSD_512x512/solver_train.prototxt -gpu 0 \
 #--snapshot=../snapshot/det_iter_110000.solverstate
