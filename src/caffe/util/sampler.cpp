1f9cc4814cd85fe948242c995577e862451d1f4a
tonny Yu <tonny.yu@honeywell.com>
2020-05-06 12:03:54 +0800

    ...crop bug...

--- src/caffe/util/sampler.cpp
+++ src/caffe/util/sampler.cpp
@@ -345,7 +345,7 @@ void GenerateDataAnchorSample(const AnnotatedDatum& anno_datum,
   }
   float w_off = 0.0f, h_off = 0.0f;
 
-  float sample_bbox_size = (float)scaleChoose * img_width / bbox_width;
+  float sample_bbox_size = img_width * std::pow(scaleChoose, 2) / bbox_aera;
   if(sample_bbox_size < COMPAREMAX(img_height, img_width)){
     if(bbox_width <= sample_bbox_size){
       caffe_rng_uniform(1, xmin + bbox_width - sample_bbox_size, xmin, &w_off);
@@ -366,11 +366,12 @@ void GenerateDataAnchorSample(const AnnotatedDatum& anno_datum,
   sampled_bbox->set_xmax((float)(w_off + sample_bbox_size) / img_width);
   sampled_bbox->set_ymax((float)(h_off + sample_bbox_size) / img_height);
   LOG(INFO)<<"CropImage Size: "<<sample_bbox_size <<
+             ", Original Width: "<<img_width<<
              ", Scale Choose: "<<scaleChoose<<
+             ", Area Size: "<<std::pow(scaleChoose, 2)<<
              ", Original Bbox height: "<<bbox_height<<
-             ", Original Bbox width: "<<bbox_width<<
-             ", Original Height: "<<img_height<<
-             ", Original Width: "<<img_width;
+             ", Original Bbox width: "<<bbox_width;
+             
 }
 
 void GenerateBatchDataAnchorSamples(const AnnotatedDatum& anno_datum,
