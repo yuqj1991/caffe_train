392e402db35f446000c09a1c1c32c4b6b1013db5
Yu <Tonny.Yu@resideo.com>
2020-05-15 14:17:26 +0800

    //

--- python/caffe/mobilenetv2_lib.py
+++ python/caffe/mobilenetv2_lib.py
@@ -147,13 +147,13 @@ def MBottleConvBlock(net, from_layer, id, repeated_num, fileter_channels, stride
         return out_layer_projects
 
 
-def ResConnectBlock(net, from_layer_one, from_layer_two, stage_idx, use_global_stats, output, use_bn, use_relu, layerPrefix):
+def ResConnectBlock(net, from_layer_one, from_layer_two, stage_idx,  use_relu, layerPrefix):
     res_name = "ResConnect_stage_{}".format(stage_idx)
     net[res_name] = L.Eltwise(net[from_layer_one], net[from_layer_two], operation = P.Eltwise.SUM)
     out_layer = "{}_stage_{}".format(layerPrefix, stage_idx)
-    ConvBNLayer(net, res_name, out_layer, use_bn = use_bn, use_relu = use_relu, 
-                num_output= output, kernel_size=1, pad = 0, 
-                stride=1, use_scale = use_bn, lr_mult=1, use_global_stats= use_global_stats)
+    if use_relu:
+        Relu_layer = L.ReLU6(net[out_layer], in_place = True)
+        out_layer = Relu_layer
     return res_name, out_layer
 
 
@@ -340,8 +340,7 @@ def CenterFaceMobilenetV2Body(net, from_layer, Use_BN = True, use_global_stats= 
         
         # eltwise_sum layer
         _, detect_layer = ResConnectBlock(net, Reconnect_layer_one, Reconnect_layer_two, channel_stage, 
-                                            use_global_stats=use_global_stats, output= fpn_out_channels,
-                                            use_bn= True, use_relu=True, layerPrefix = "Res_conv_linear")
+                                            use_relu=True, layerPrefix = "Res_conv_linear")
         out_layer = detect_layer
 
     ### class prediction layer
@@ -480,14 +479,11 @@ def CenterGridMobilenetV2Body(net, from_layer, Use_BN = True, use_global_stats= 
                 num_output= fpn_out_channels, kernel_size= 3, pad= 1, stride= 2,
                 lr_mult=1, use_scale= True, use_global_stats= use_global_stats)
             Res_Layer_two = LayerList_Name[len(feature_stride) - index - 1]
-            _, Reconnect_layer_two = ResConnectBlock(net, Res_Layer_one, Res_Layer_two, 
-                                                        index, use_global_stats=use_global_stats, output= fpn_out_channels, use_bn= True,
-                                                        use_relu=False, layerPrefix = "FPN_linear_".format(index))
+            _, Reconnect_layer_two = ResConnectBlock(net, Res_Layer_one, Res_Layer_two, index, use_relu=False, layerPrefix = "FPN_linear_".format(index))
         else:
             Reconnect_layer_two= LayerList_Name[len(feature_stride) - index - 1]
         # eltwise_sum layer
-        out_layer, detect_layer = ResConnectBlock(net, Reconnect_layer_one, Reconnect_layer_two, channel_stage, use_global_stats=use_global_stats, output= 6,
-                                                    use_bn= False, use_relu=False, layerPrefix = "Dectction")
+        out_layer, detect_layer = ResConnectBlock(net, Reconnect_layer_one, Reconnect_layer_two, channel_stage, use_relu=False, layerPrefix = "Dectction")
         LayerList_Output.append(detect_layer)
     return net, LayerList_Output
 
