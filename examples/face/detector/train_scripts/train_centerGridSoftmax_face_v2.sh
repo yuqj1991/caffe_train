b09882fdc71453cb49b7cfbd267e89b804e82227
yuqj1990 <yuqj1990@deepano.com>
2020-05-18 14:01:41 +0800

    ...

--- examples/face/detector/train_scripts/train_centerGridSoftmax_face_v2.sh
+++ examples/face/detector/train_scripts/train_centerGridSoftmax_face_v2.sh
@@ -2,10 +2,12 @@
 if ! test -f ../prototxt/Full_640x640/CenterGridSoftmax_face_v2_train.prototxt ;then 
    echo "error: ../prototxt/Full_640x640/CenterGridSoftmax_face_v2_train.prototxt does not exit." 
    echo "please generate your own model prototxt primarily." 
-   exit 1 
+   exit 1
+fi
 if ! test -f ../prototxt/Full_640x640/CenterGridSoftmax_face_v2_test.prototxt ;then 
    echo "error: ../prototxt/Full_640x640/CenterGridSoftmax_face_v2_test.prototxt does not exit." 
    echo "please generate your own model prototxt primarily." 
-   exit 1 
+   exit 1
+fi
 ../../../../build/tools/caffe train --solver=../prototxt/Full_640x640/CenterGridSoftmax_face_v2_solver.prototxt --gpu 1 \
 # --snapshot=../snapshot/CenterGridSoftmax_face_v2_iter_5000.solverstate 
