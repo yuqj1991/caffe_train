cac7f3b0364dfec34e94c44e50a77dc6c4a6f0a4
yuqj1990 <yuqj1990@deepano.com>
2020-05-19 18:36:43 +0800

    ...

--- examples/face/detector/train_scripts/train_center_face_v2.sh
+++ examples/face/detector/train_scripts/train_center_face_v2.sh
@@ -2,12 +2,10 @@
 if ! test -f ../prototxt/Full_640x640/Centerface_v2_train.prototxt ;then 
    echo "error: ../prototxt/Full_640x640/Centerface_v2_train.prototxt does not exit." 
    echo "please generate your own model prototxt primarily." 
-   exit 1
-fi
+   exit 1 
 if ! test -f ../prototxt/Full_640x640/Centerface_v2_test.prototxt ;then 
    echo "error: ../prototxt/Full_640x640/Centerface_v2_test.prototxt does not exit." 
    echo "please generate your own model prototxt primarily." 
-   exit 1
-fi
+   exit 1 
 ../../../../build/tools/caffe train --solver=../prototxt/Full_640x640/Centerface_v2_solver.prototxt --gpu 0 \
 # --snapshot=../snapshot/Centerface_v2_iter_5000.solverstate 
