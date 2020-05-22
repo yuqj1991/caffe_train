419c0b281cb3b3c8f82e4c6092b9d0e0f4a7520d
yuqj1990 <yuqj1990@deepano.com>
2020-05-22 17:21:10 +0800

    try to merge conv & batch_norm & scale to reduce gpu memory

--- examples/face/detector/eval/scripts/demo_image.py
+++ examples/face/detector/eval/scripts/demo_image.py
@@ -66,10 +66,10 @@ def detect(imgfile):
            p2 = (box[i][2], box[i][3])
            cv2.rectangle(origimg, p1, p2, (0,255,0))
            p3 = (max(p1[0], 15), max(p1[1], 15))
-           title = "%s:%.2f" % (CLASSES[int(cls[i])], conf[i])
-           cv2.putText(origimg, title, p3, cv2.FONT_ITALIC, 0.6, (0, 255, 0), 1)
+           #title = "%s:%.2f" % (CLASSES[int(cls[i])], conf[i])
+           #cv2.putText(origimg, title, p3, cv2.FONT_ITALIC, 0.6, (0, 255, 0), 1)
     name = test_dir + '/' + imgfile.split("/")[-1].split('.jpg')[0] + '_crop.jpg'
-    cv2.imwrite(name, origimg)
+    #cv2.imwrite(name, origimg)
     cv2.imshow("facedetector", origimg)
  
     k = cv2.waitKey(0) & 0xff
