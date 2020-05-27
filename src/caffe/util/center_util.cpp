1ffae73c1f46ab3ddca7e0e9315400b8585e624d
yuqj1990 <yuqj@deepano>
2020-05-27 14:22:26 +0800

    ...

--- src/caffe/util/center_util.cpp
+++ src/caffe/util/center_util.cpp
@@ -323,7 +323,7 @@ Dtype SoftmaxLossEntropy(Dtype* label_data, Dtype* pred_data,
                     Dtype pred_another_data_value = std::exp(pred_data[bg_index + (1 - label_idx) * dimScale] - MaxVaule) / sumValue;
                     loss -= log(std::max(pred_data_value,  Dtype(FLT_MIN)));
                     bottom_diff[bg_index + label_idx * dimScale] = pred_data_value - 1;
-                    bottom_diff[bg_index + (1 - label_idx) * dimScale] = pred_another_data_value - 1;
+                    bottom_diff[bg_index + (1 - label_idx) * dimScale] = pred_another_data_value;
                 }
             }
         }
