2f9502cb1da78f457d4b802cb85b4eb43f78155b
yuqj1990 <yuqj1990@deepano.com>
2020-05-31 13:28:10 +0800

    ,,,

--- examples/face/detector/train_scripts/create_data_anno.sh
+++ examples/face/detector/train_scripts/create_data_anno.sh
@@ -20,5 +20,5 @@ then
 fi
 for subset in wider_train wider_val
 do
-  python3 create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir ../examples/face/detector/train_scripts/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_"$db $data_root_dir/$dataset_name
+  python3 create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir ../examples/face/detector/train_scripts/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_lm_"$db $data_root_dir/$dataset_name
 done
