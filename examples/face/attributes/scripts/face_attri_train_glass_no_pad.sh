5e852ebf9217a9609f0e52b9068bca7ba3105fde
yuqj1990 <yuqj1990@deepano.com>
2020-06-06 14:45:54 +0800

    add wulai version

--- examples/face/attributes/scripts/face_attri_train_glass_no_pad.sh
+++ examples/face/attributes/scripts/face_attri_train_glass_no_pad.sh
@@ -9,5 +9,5 @@ if ! test -f ../prototxt/test_glass_no_pad.prototxt ;then
 	echo "please generate your own model prototxt primarily."
         exit 1
 fi
-../../../../build/tools/caffe train --solver=../solver/solver_glass_no_pad.prototxt -gpu 0 \
---snapshot=../snapshot/face_glass_iter_377459.solverstate
+../../../../build/tools/caffe train --solver=../solver/solver_glass_no_pad.prototxt -gpu 1 \
+#--snapshot=../snapshot/face_glass_iter_377459.solverstate
