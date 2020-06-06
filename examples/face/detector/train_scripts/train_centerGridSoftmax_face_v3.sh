3eae500a20b22ece3ac00d042951fadf7bca4a27
yuqj1990 <yuqj1990@deepano.com>
2020-06-06 10:28:03 +0800

    ...

--- examples/face/detector/train_scripts/train_centerGridSoftmax_face_v3.sh
+++ examples/face/detector/train_scripts/train_centerGridSoftmax_face_v3.sh
@@ -1,13 +1,13 @@
 #!/bin/sh 
-if ! test -f ../prototxt/Full_640x640/CenterGridSoftmax_face_v3_train.prototxt ;then 
-   echo "error: ../prototxt/Full_640x640/CenterGridSoftmax_face_v3_train.prototxt does not exit." 
+if ! test -f ../prototxt/Full_320x320/CenterGridSoftmax_face_v3_train.prototxt ;then 
+   echo "error: ../prototxt/Full_320x320/CenterGridSoftmax_face_v3_train.prototxt does not exit." 
    echo "please generate your own model prototxt primarily." 
    exit 1
 fi
-if ! test -f ../prototxt/Full_640x640/CenterGridSoftmax_face_v3_test.prototxt ;then 
-   echo "error: ../prototxt/Full_640x640/CenterGridSoftmax_face_v3_test.prototxt does not exit." 
+if ! test -f ../prototxt/Full_320x320/CenterGridSoftmax_face_v3_test.prototxt ;then 
+   echo "error: ../prototxt/Full_320x320/CenterGridSoftmax_face_v3_test.prototxt does not exit." 
    echo "please generate your own model prototxt primarily." 
    exit 1
 fi
-../../../../build/tools/caffe train --solver=../prototxt/Full_640x640/CenterGridSoftmax_face_v3_solver.prototxt --gpu 1 \
+../../../../build/tools/caffe train --solver=../prototxt/Full_320x320/CenterGridSoftmax_face_v3_solver.prototxt --gpu 1 \
 # --snapshot=../snapshot/CenterGridSoftmax_face_v3_0_iter_5000.solverstate 
