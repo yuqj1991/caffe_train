3549d14cbfbe23140b7dc5ed1ba8e31f540916ce
yuqj1990 <yuqj@deepano>
2020-06-19 11:39:00 +0800

    ...

--- examples/face/detector/train_scripts/train_center_face_v2.sh
+++ examples/face/detector/train_scripts/train_center_face_v2.sh
@@ -10,5 +10,4 @@ if ! test -f ../prototxt/Full_640x640/Centerface_v2_test.prototxt ;then
    exit 1 
 fi
 ../../../../build/tools/caffe train --solver=../prototxt/Full_640x640/Centerface_v2_solver.prototxt --gpu 0 \
---weights= ../mobilenet_v2.caffemodel
 # --snapshot=../snapshot/Centerface_v2_0_iter_5000.solverstate 
