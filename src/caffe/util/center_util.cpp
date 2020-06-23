4022354e77b2374c28f48d0d3cefba537bfbaf33
tonny Yu <Tonny.Yu@resideo.com>
2020-06-23 12:11:24 +0800

    remove some deprecated net

--- src/caffe/util/center_util.cpp
+++ src/caffe/util/center_util.cpp
@@ -315,9 +315,9 @@ Dtype FocalLossSoftmax(Dtype* label_data, Dtype* pred_data,
 
                     #if 1
                     loss -= alpha * std::pow(p0, gamma) * std::log(std::max(p1,  Dtype(FLT_MIN)));
-                    bottom_diff[bg_index + label_idx * dimScale] = (alpha) * std::pow(p0, gamma) * 
+                    bottom_diff[bg_index + label_idx * dimScale] = alpha * std::pow(p0, gamma) * 
                                                                 (gamma * std::log(std::max(p1,  Dtype(FLT_MIN))) * p1 - p0);
-                    bottom_diff[bg_index + (1 - label_idx) * dimScale] = (alpha) * std::pow(p0, gamma) * 
+                    bottom_diff[bg_index + (1 - label_idx) * dimScale] = alpha * std::pow(p0, gamma) * 
                                                                 (p0 - gamma * std::log(std::max(p1,  Dtype(FLT_MIN))) * p1 );
                     #else
                     Dtype assist_value = Dtype((p0 + transfer) / (p1 + transfer));
