597b949d82faed140a38fb6b8532e1337463f19e
tonny Yu <Tonny.Yu@resideo.com>
2020-07-10 14:20:10 +0800

    buggggggggggggggggggggggggg

--- src/caffe/layers/batch_norm_layer.cu
+++ src/caffe/layers/batch_norm_layer.cu
@@ -10,7 +10,7 @@ template <typename Dtype>
 __global__ void batchNorm_variance(int nthreads, int width, int height, int channels, const Dtype* bottom_data, Dtype* top_data,Dtype* var_data){
     CUDA_KERNEL_LOOP(index, nthreads){
         const int fc = (index / width / height) % channels;
-        printf("index: &d, fc: %d, bottom_data: %lf, top_data: %lf\n", index, fc, bottom_data[index], top_data[index]);
+        printf("width: %d, height: %d, index: %d, fc: %d, bottom_data: %lf, top_data: %lf\n", width, height, index, fc, bottom_data[index], top_data[index]);
         var_data[fc] += pow(top_data[index], 2.);
     }
 }
